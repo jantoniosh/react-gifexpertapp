0eb20d3b6515f8f8ca8a8694dedfe8f82acd12ec
Antonio Salinas <salinash.jantonio@gmail.com>
2020-07-04 15:12:07 -0500

    index.html actualizado

---
+++ docs/precache-manifest.af8b7a80f5508762efccdfdccdbeec21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2397646f27a7d55c169a2a5c03b863",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8e98b9008f50245492b",
+    "url": "/static/css/main.0c640f5e.chunk.css"
+  },
+  {
+    "revision": "a88b195f1dccd4342fee",
+    "url": "/static/js/2.d6db1626.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6db1626.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e98b9008f50245492b",
+    "url": "/static/js/main.c741dacb.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
